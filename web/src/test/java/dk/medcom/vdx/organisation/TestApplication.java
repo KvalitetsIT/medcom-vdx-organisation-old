f2bd6910b7bad8b622c5dd67fd909e351d04a900
Jonas Pedersen <jpe@kvalitetsit.dk>
2020-03-25 16:46:15 +0100

    Test application

---
+++ web/src/test/java/dk/medcom/vdx/organisation/TestApplication.java
@@ -0,0 +1,27 @@
+package dk.medcom.vdx.organisation;
+
+import org.springframework.boot.SpringApplication;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.Network;
+
+public class TestApplication {
+    public static void main(String[] args) {
+        Network n = Network.newNetwork();
+        MySQLContainer mysql = (MySQLContainer) new MySQLContainer("mysql:5.7")
+                .withDatabaseName("orgdb")
+                .withUsername("orguser")
+                .withPassword("secret1234")
+                .withNetwork(n);
+
+        mysql.start();
+        String jdbcUrl = mysql.getJdbcUrl();
+        System.setProperty("jdbc.url", jdbcUrl);
+        System.setProperty("usercontext.header.name", "e");
+        System.setProperty("userattributes.role.key", "d");
+        System.setProperty("userrole.admin.values", "c");
+        System.setProperty("userrole.user.values", "b");
+        System.setProperty("userattributes.org.key", "a");
+
+        SpringApplication.run(Application.class, args);
+    }
+}
